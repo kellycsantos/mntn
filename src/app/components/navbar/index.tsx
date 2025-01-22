ebc093dd35a44e4bda8f0c43cb4f2d897588cdd3
Kelly C Santos <kellycristinasntos@gmail.com>
2025-01-22 01:55:21 -0300

    add link and aria label to logo

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -10,33 +10,35 @@ export default function Navbar() {
 
         let nav = document.getElementById('menu')
         visible === false ? setVisible(true) : setVisible(false)
-    
 
-}
 
-useEffect(() => {
-    if (typeof window !== 'undefined') {
-        setVisible(false)
     }
 
-}, [])
-return (
-    <nav id={styles.nav}>
-        <img src={logo.src} alt='' />
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setVisible(false)
+        }
 
-        <ul id="menu" className={!visible ? `${styles.closed}` : ''}>
-            <li><a href="/equipaments">Equipment</a></li>
-            <li><a href="/about-us">About us</a></li>
-            <li><a href="/news">Blog</a></li>
-            <li><a href="/">Login</a></li>
-        </ul>
-        <span>
-            <img src={account.src} alt="Account" />
-            <a href="#">
-                Login
+    }, [])
+    return (
+        <nav id={styles.nav}>
+            <a href="/" aria-label='go to home'>
+                <img src={logo.src} alt='icon mntn' />
             </a>
-        </span>
-        <button className={!visible ? `${styles.closed}` : ''} onClick={(e) => showMenu()} aria-label={visible ? 'close menu' : 'open menu'}></button>
-    </nav>
-)
+
+            <ul id="menu" className={!visible ? `${styles.closed}` : ''}>
+                <li><a href="/equipaments">Equipment</a></li>
+                <li><a href="/about-us">About us</a></li>
+                <li><a href="/news">Blog</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+            <span>
+                <img src={account.src} alt="Account" />
+                <a href="#">
+                    Login
+                </a>
+            </span>
+            <button className={!visible ? `${styles.closed}` : ''} onClick={(e) => showMenu()} aria-label={visible ? 'close menu' : 'open menu'}></button>
+        </nav>
+    )
 }
