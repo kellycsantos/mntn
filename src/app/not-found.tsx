e344f7595435dd9744a19d60cf5ddafa0ab1168f
Kelly C Santos <kellycristinasntos@gmail.com>
2025-01-22 00:22:15 -0300

    created not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client';
+import notFoundBg from '../assets/not-found-bg.webp'
+import styles from './not-found.module.scss'
+export default function notFound(){
+    return(
+        <section className={styles.notfound} >
+        <h1>Oh no, you've gone the wrong way!</h1>
+        <h2>Shall we go back to the <a href="/">beginning</a>?</h2>
+        <img src={notFoundBg.src} alt={''} />
+        </section>
+    )
+}
